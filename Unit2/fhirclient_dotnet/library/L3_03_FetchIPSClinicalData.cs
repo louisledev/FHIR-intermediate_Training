51a409a65b76f83a722d1d7c08046b0f26fafd35
Louis Latour <louis@fire.ly>
2024-03-15 08:43:22 +0100

    Fixing the NO_IPS test

--- Unit2/fhirclient_dotnet/library/L3_03_FetchIPSClinicalData.cs
+++ Unit2/fhirclient_dotnet/library/L3_03_FetchIPSClinicalData.cs
@@ -25,10 +25,17 @@ namespace fhirclient_dotnet
                 return "Error:No_IPS";
             }
 
+            if (CheckIfIPSEmpty(bundle))
+            {
+                return "Error:No_IPS";
+            }
+            
             var medicationStatements = bundle.Entry
                 .Where(e => e.Resource is MedicationStatement)
                 .Select(e => e.Resource as MedicationStatement);
 
+            
+            
             var result = "";
             foreach (var ms in medicationStatements)
             {
@@ -49,7 +56,7 @@ namespace fhirclient_dotnet
             }
             return result;
         }
-
+        
         public string GetIPSImmunizations
         (string ServerEndPoint,
             string IdentifierSystem,
@@ -68,6 +75,11 @@ namespace fhirclient_dotnet
                 return "Error:No_IPS";
             }
 
+            if (CheckIfIPSEmpty(bundle))
+            {
+                return "Error:No_IPS";
+            }
+            
             var immunizations = bundle.Entry
                 .Where(e => e.Resource is Immunization)
                 .Select(e => e.Resource as Immunization)
@@ -87,9 +99,48 @@ namespace fhirclient_dotnet
                 result += $"{immunization!.Status}|{immunization?.Occurrence}|{immunization!.VaccineCode?.Coding.FirstOrDefault()?.Code}:{immunization.VaccineCode?.Coding.FirstOrDefault()?.Display}\n";
             }
             
-            
             return result;
-
         }
+        
+        private static bool CheckIfIPSEmpty(Bundle bundle)
+        {
+            // Check if the bundle contains a MedicationStatement with status=unknown and medicationCodeableConcept="No information about medication" and
+            // an AllergyIntolerance with clinicalStatus=active and code=no-allergy-info/"No information about allergies" and
+            // a Condition with clinicalStatus=active and code=no-problem-info/"No information about problems"
+            
+            var noMedicationStatement = bundle.Entry
+                .Where(e => e.Resource is MedicationStatement)
+                .Select(e => e.Resource as MedicationStatement)
+                .Where(ms => ms != null)
+                .Any(ms => ms!.Status == MedicationStatement.MedicationStatusCodes.Unknown &&
+                           ms.Medication is CodeableConcept cc &&
+                           cc.Coding.FirstOrDefault()?.Code == "no-medication-info");
+            if (!noMedicationStatement)
+                return false;
+            
+            var noAllergyIntolerance = bundle.Entry
+                .Where(e => e.Resource is AllergyIntolerance)
+                .Select(e => e.Resource as AllergyIntolerance)
+                .Where(ai => ai != null)
+                .Any(ai => ai!.Code is { } cc &&
+                           cc.Coding.FirstOrDefault()?.Code == "no-allergy-info" &&
+                           ai.ClinicalStatus.Coding.FirstOrDefault()?.Code == "active");
+            if (!noAllergyIntolerance)
+                return false;
+            
+            var noCondition = bundle.Entry
+                .Where(e => e.Resource is Condition)
+                .Select(e => e.Resource as Condition)
+                .Where(c => c != null)
+                .Any(c => c!.Code is { } cc &&
+                          cc.Coding.FirstOrDefault()?.Code == "no-problem-info" &&
+                          c.ClinicalStatus.Coding.FirstOrDefault()?.Code == "active");
+
+            if (!noCondition)
+                return false;
+
+            return true;
+        }
+
     }
 }
